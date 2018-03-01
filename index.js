80518608e1f9a7c244b379757f3af59db3f5f0fa
amartinr1977 <amartinromero@gmail.com>
2018-03-01 13:26:00 +0100

    version 0.0.3 - plugin coolimages

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ module.exports = {
       process: function (block) {
         url_image = block.kwargs.url_image;
         textimage = block.body;
-        element = '<div class="container">' +
-          '<img src="' + url_image + '" alt="Norway" width="500" height="300">' +
-          '<div class="imagetext"><tt>' + textimage + '</tt></div>' +
-          '</div>' ;
+        element = '<div class="container">';
+        element += '<img src="' + url_image + '" alt="Norway" width="500" height="300">';
+        element += '<div class="imagetext"><tt>' + textimage + '</tt></div>';
+        element += '</div>';
 
         return element;
       }
