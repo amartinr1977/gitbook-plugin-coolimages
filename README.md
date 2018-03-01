ce279543cf73c82368b1008c3c067bf77d26b848
amartinr1977 <amartinromero@gmail.com>
2018-03-01 12:48:07 +0100

    ok

---
+++ README.md
@@ -0,0 +1,96 @@
+# Plugin for GitBook: Notificaciones 
+
+Este plugin va a permitir al usuario final introducir cuadros de dialogo o notificaciones personalizadas con un aspecto más atractivo que el que permite el codigo **mardown** por defecto.
+
+## ¿Qué aspecto tiene?
+
+El aspecto que tendrían los diálogos que incorparía el **Plugin Notificaciones** en tu **GitBook** sería similar al siguiente:
+
+![Ejemplo de Notificación de Tipo Tarea](https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master/assets/images/notificacion_task-example.png?raw=true)
+
+## ¿Cómo hacer uso del Plugin?
+
+Para hacer uso de este **Plugin de GitBook** tan sólo tienes que editar el archivo **book.json** de tu GitBook y añadir el siguiente campo:
+
+```json
+{
+    "plugins": [
+        "notificaciones"
+    ]
+}
+```
+
+Una vez configurado el archivo anterior tan sólo será necesario hacer uso del plugin como se muestra a continuación, dependiendo del tipo de dialogo o notificación a mostrar.
+
+## Ejemplos de uso:
+
+Las siguientes imágenes y trozos de código muestran como se configuraría una notificación en tu **GitBook**:
+
+### Notificación de tipo Tarea
+
+```
+{% notificacion_task title="Título de la Tarea", 
+    numexer="1.1.2", 
+    req="Requisitos para realizar la Tarea",
+    formatoentrega="El formato de entraga de la solución del ejercicio debe ser ..." %}
+La tarea a realizar consiste en lo que indicaremos en el cuerpo de la notificación.  Por ejemplo, podemos poner una lista de acciones a realizar, haciendo uso de una lista ordenada o desordenada mediante lenguaje <b>HTML/CSS</b>.  En el caso de una lista ordenada el aspecto que se mostraría es el siguiente:
+<br/>
+<ol>
+<li>Deberás hacer <b>una captura de pantalla</b> de ...</li>
+<li>Deberás <b>registrarte</b> en ...</li>
+</ol>
+<br/>
+O una desordenada:
+<br/>
+<ul>
+<li>Deberás llevar a cabo ...</li>
+<li>Y por último ...</li>
+</ul>
+{% endnotificacion_task %}
+```
+
+Su aspecto resultante sería:
+
+![Ejemplo de Notificación de Tipo Tarea](https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master/assets/images/notificacion_task-example.png?raw=true)
+
+### Notificación de tipo _Alerta_
+
+El código a usar en **GitBook** para hecer uso de este diálogo del **Plugin** sería uno similar al siguiente:
+
+```
+{% notificacion_alert title="¡¡Título de la Alerta!!", logotext="¡¡Aviso!!" %}
+El texto que se mostrará en la alerta será el que indiquemos en el cuerpo de la notificación.  Este tipo de diálogo o notificación puede resultar útil cuando queremos advertir a un usuario de algún aspecto que estemos tratando.
+{% endnotificacion_alert %}
+```
+
+Su aspecto resultante sería:
+
+![Ejemplo de Notificación de Tipo Alerta](https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master/assets/images/notificacion_alert-example.png?raw=true)
+
+### Notificación de tipo _¿Sabías qué ...?_
+
+El código a usar en **GitBook** para hecer uso de este diálogo del **Plugin** sería uno similar al siguiente:
+
+```
+{% notificacion_didyouknow title="¿Sabías que ...??" %}
+Este diálogo puede servirnos para enfatizar algo que consideramos que puede ser interesante que sepa el usuario final.  Al igual que en los diálogos o notificaciones anteriores puede hacerse uso de etiquetado <b>HTML/CSS</b> para mejorar el aspecto del cuerpo del contenido de la notificación.
+{% endnotificacion_didyouknow %}
+```
+
+Su aspecto resultante sería:
+
+![Ejemplo de Notificación de Tipo didyouknow](https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master/assets/images/notificacion_didyouknow-example.png?raw=true)
+
+### Notificación de tipo _Important_
+
+El código a usar en **GitBook** para hecer uso de este diálogo del **Plugin** sería uno similar al siguiente:
+
+```
+{% notificacion_important title="Lo más importante es que el Código es Libre" %}
+Este diálogo puede servirnos para <b>enfatizar los aspectos más importantes</b> de todo lo que le queremos mostrar al usuario final.  Al igual que en los diálogos o notificaciones anteriores puede hacerse uso de etiquetado <b>HTML/CSS</b> para mejorar el aspecto del cuerpo del contenido de la notificación.
+{% endnotificacion_important %}
+```
+
+Su aspecto resultante sería:
+
+![Ejemplo de Notificación de Tipo Importante](https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master/assets/images/notificacion_important-example.png?raw=true)
---
+++ index.js
@@ -0,0 +1,109 @@
+module.exports = {
+
+  book: {
+
+    assets: './assets',
+
+    css: [
+      'notificaciones.css'
+    ]
+  },
+
+  blocks: {
+    notificacion_task: {
+      process: function (block) {
+        ruta_base = "https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master";
+        logo_aviso = '<img src="' + ruta_base + '/assets/images/logobombilla-x80.png?raw=true"></img>';
+        logo_texto = 'Tarea</br>' + block.kwargs.numexer;
+
+        element = '<div class="contenedor contenedor-task">';
+        // Indicamos el logo o imagen de la notificación relativa a: Tarea
+        element += '<div class="image">' + logo_aviso + '</br><b><tt>' + logo_texto + '</tt></b></div>';
+        element += '<div class="contenedor-titulo"><div class="titulo titulo-ejercicio"><b><tt>' + block.kwargs.title + '</tt></b>';
+        element += '<div class="requisitos">';
+        // Establecemos los Requisitos necesarios para realizar la tarea
+        element += '<b><i><tt>Requisitos: <span class="textoreq">' + block.kwargs.req + '</span>';
+        element += '</b></i></tt></div>';
+        element += '</div></div>';
+        // Agregamos el cuerpo o contenido de la tarea a realizar
+        element += '<div class="contenido">' + block.body + '</div>';
+        // Establecemos el formato de entrega del ejercicio:
+        element += '<div class="formato_entrega">';
+        element += '<tt><b>Formato de Entrega:</b> ' + block.kwargs.formatoentrega + '</tt>';
+        element += '</div></div>';
+
+        return element;
+      }
+    },
+    notificacion_didyouknow: {
+      process: function (block) {
+        ruta_base = "https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master";
+        logo_aviso = '<img src="' + ruta_base + '/assets/images/logoaviso-x80.png?raw=true"></img>';
+        logo_texto = '¿¿Sabías qué??';
+
+        element = '<div class="contenedor">';
+        element += '<div class="image">' + logo_aviso + '</br><b><tt>' + logo_texto + '</tt></b></div>';
+        element += '<div class="contenedor-titulo"><div class="titulo titulo-aviso"><b><tt>' + block.kwargs.title + '</tt></b></div></div>';
+        element += '<div class="contenido">' + block.body + '</div>';
+        element += '</div>';
+
+        return element;
+      }
+    },
+    notificacion_important: {
+      process: function (block) {
+        ruta_base = "https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master";
+        logo_lupa = '<img src="' + ruta_base + '/assets/images/logolupa-x80.png?raw=true"></img>';
+        logo_texto = '¡¡Importante!!';
+        
+        element = '<div class="contenedor">';
+        element += '<div class="image">' + logo_lupa + '</br><b><tt>' + logo_texto + '</tt></b></div>';
+        element += '<div class="contenedor-titulo"><div class="titulo titulo-lupa"><b><tt>' + block.kwargs.title + '</tt></b></div></div>';
+        element += '<div class="contenido">' + block.body + '</div>';
+        element += '</div>';
+
+        return element;
+      }
+    },
+    notificacion_alert: {
+      process: function (block) {
+        logo_alert = '<img src="https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master/assets/images/logoalerta-x80.png?raw=true"></img>';
+        logo_texto = block.kwargs.logotext;
+
+        element = '<div class="contenedor">';
+        element += '<div class="image">' + logo_alert + '</br><b><tt>' + logo_texto + '</tt></b></div>';
+        element += '<div class="contenedor-titulo"><div class="titulo titulo-alert"><b><tt>' + block.kwargs.title + '</tt></b></div></div>';
+        element += '<div class="contenido">' + block.body + '</div>';
+        element += '</div>';
+
+        return element;
+      }
+    },
+    notificacion_table: {
+      process: function (block) {
+        logo_info = '<img src="https://github.com/amartinr1977/gitbook-plugin-notificaciones/blob/master/assets/images/logoalerta-x80.png?raw=true"></img>';
+        formato_entrega = "Esto es el formato de entrega ...";
+        requisitos = "Los requisitos van a ser ...";
+        element = '<div class="">';
+        element += '<table>';
+        element += '<tr>';
+        element += '<td rowspan="3">' + logo_info + '<br>' + block.kwargs.numexer + '</td>';
+        element += '<td>' + block.kwargs.title + '</td>';
+        element += '</tr>';
+        element += '<tr>';
+        element += '<td>' + requisitos + '</td>';
+        element += '</tr>';
+        element += '<tr>';
+        element += '<td>' + block.body + '</td>';
+        element += '</tr>';
+        element += '<tr>';
+        element += '<td colspan="2">' + formato_entrega + '</td>';
+        element += '</tr>';
+        element += '</table>';
+        element += '</div>';
+        return element;
+      }
+    }
+  }
+
+};
